392daf3eabe5fa2b05d7c093aabca528c2ddef31
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-06-17 20:57:30 -0300

    ⚙️ created public guard for public endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './resources/auth/guards/jwt-auth.guard';
+import { PublicGuard } from './resources/auth/guards/public.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Use global validation pipe
   app.useGlobalPipes(new ValidationPipe());
-
-  // Apply the JWT Auth Guard globally
-  app.useGlobalGuards(new JwtAuthGuard());
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new PublicGuard(reflector), new JwtAuthGuard(reflector));
 
   await app.listen(3000);
 }
---
+++ src/resources/auth/guards/public.guard.ts
@@ -0,0 +1,12 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+
+@Injectable()
+export class PublicGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    return this.reflector.get<boolean>(IS_PUBLIC_KEY, context.getHandler());
+  }
+}
