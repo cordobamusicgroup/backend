4964c804959cd797b550a20c358dc3acb50dd49f
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-06-17 20:57:43 -0300

    ⚙️ implement of login dto

--- src/resources/auth/auth.controller.ts
+++ src/resources/auth/auth.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { Public } from './decorators/public.decorator';
+import { AuthLoginDto } from './dto/auth-login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
+  async login(@Body() authLoginDto: AuthLoginDto) {
+    return this.authService.login(authLoginDto);
   }
 }
--- src/resources/auth/auth.service.ts
+++ src/resources/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 import { User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
 import { JwtPayloadDto } from './dto/jwt-payload.dto';
+import { AuthLoginDto } from './dto/auth-login.dto';
 
 @Injectable()
 export class AuthService {
@@ -20,7 +21,12 @@ export class AuthService {
     return null;
   }
 
-  async login(user: User) {
+  async login(authLoginDto: AuthLoginDto) {
+    const { username, password } = authLoginDto;
+    const user = await this.validateUser(username, password);
+    if (!user) {
+      return new UnauthorizedException('Invalid credentials');
+    }
     const payload: JwtPayloadDto = {
       username: user.username,
       sub: user.id,
