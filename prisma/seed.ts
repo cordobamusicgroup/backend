db594e0a140e62fbdfdb4ccac6827c42a4da974c
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-06-22 23:12:15 -0300

    ⚙️ added seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,47 +1,66 @@
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { countries } from './countries-seed';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  // Crear algunos países
-  const argentina = await prisma.country.create({
-    data: {
-      name: 'Argentina',
-      code: 'ARG',
-      continent: 'AMERICA',
-    },
+  for (const country of countries) {
+    await prisma.country.create({
+      data: {
+        name: country.name,
+        shortCode: country.shortCode,
+        code: country.code,
+        continent: country.continent,
+      },
+    });
+  }
+
+  const argentina = await prisma.country.findUnique({
+    where: { code: 'ARG' },
   });
 
-  const address1 = await prisma.address.create({
-    data: {
-      street: 'Sargento Cabral 241',
-      city: 'Cruz del Eje',
-      state: 'Cordoba',
-      zip: '5280',
-      countryId: argentina.id,
-    },
-  });
+  if (argentina) {
+    const address1 = await prisma.address.create({
+      data: {
+        street: 'Sargento Cabral 241',
+        city: 'Cruz del Eje',
+        state: 'Cordoba',
+        zip: '5280',
+        countryId: argentina.id,
+      },
+    });
 
-  const client1 = await prisma.client.create({
-    data: {
-      name: 'Santiago Joaquin Diaz',
-      type: 'INDIVIDUAL',
-      addressId: address1.id,
-      vatId: '123456789',
-      taxId: '987654321',
-    },
-  });
+    const client1 = await prisma.client.create({
+      data: {
+        name: 'Santiago Joaquin Diaz',
+        type: 'INDIVIDUAL',
+        addressId: address1.id,
+        vatId: '123456789',
+        taxId: '987654321',
+      },
+    });
 
-  // Crear algunos usuarios
-  await prisma.user.create({
-    data: {
-      username: 'admin',
-      email: 'admin@example.com',
-      password: 'password123',
-      role: 'ADMIN',
-      clientId: client1.id,
-    },
-  });
+    const label = await prisma.label.create({
+      data: {
+        client: { connect: { id: 1 } },
+        name: 'Legion Records',
+      },
+    });
+
+    const password = 'admin';
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await prisma.user.create({
+      data: {
+        username: 'admin',
+        email: 'admin@example.com',
+        password: hashedPassword,
+        role: 'ADMIN',
+        clientId: client1.id,
+      },
+    });
+  }
 }
 
 main()
