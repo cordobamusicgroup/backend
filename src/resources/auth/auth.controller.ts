bab1109b867103fd37567aa3c052d8d79feebe9b
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-06-27 20:01:14 -0300

    chore: Add endpoint to retrieve current user data

--- src/resources/auth/auth.controller.ts
+++ src/resources/auth/auth.controller.ts
@@ -1,17 +1,33 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from '../../common/guards/local-auth.guard';
 import { Public } from '../../common/decorators/public.decorator';
 import { AuthLoginDto } from './dto/auth-login.dto';
+import { CurrentUserResponseDto } from './dto/current-user-data.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Public()
-  @UseGuards(LocalAuthGuard)
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   async login(@Body() authLoginDto: AuthLoginDto) {
     return this.authService.login(authLoginDto);
+  }
+
+  @Get('me')
+  @HttpCode(HttpStatus.OK)
+  async me(@Request() req): Promise<CurrentUserResponseDto> {
+    return this.authService.getCurrentUserData(req.user);
   }
 }
