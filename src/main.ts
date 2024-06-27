6e4b95d0b4d141f543d2e105c7cf68acaa6dad1f
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-06-27 20:01:01 -0300

    chore: Enable CORS for multiple origins

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:6060',
+      'https://app.cmgdistro.com',
+      'https://app.cmgdistro.dev',
+    ],
+    credentials: true,
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
-  const reflector = app.get(Reflector);
-  app.useGlobalGuards(new PublicGuard(reflector), new JwtAuthGuard(reflector));
   // Configuración de Swagger
   const config = new DocumentBuilder()
     .setTitle('API Example')
@@ -23,6 +30,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(6060);
 }
 bootstrap();
