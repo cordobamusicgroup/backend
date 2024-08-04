d0ffa7a84eda816b9bcfc023b47df11250be3210
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-08-04 02:20:24 -0300

    chore: deleted old migrations

--- prisma/migrations/20240705073742_freeagent/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "freeagent_tokens" (
-    "id" SERIAL NOT NULL,
-    "accessToken" TEXT NOT NULL,
-    "refreshToken" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "freeagent_tokens_pkey" PRIMARY KEY ("id")
-);
