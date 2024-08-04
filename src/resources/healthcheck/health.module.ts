a46f05c126f5baede34844a224dda809c43ad7a4
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-08-04 03:36:17 -0300

    feat: Add HealthController and HealthModule for health check functionality

---
+++ src/resources/healthcheck/health.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TerminusModule } from '@nestjs/terminus';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaHealthIndicator } from './prisma.health';
+import { HealthController } from './health.controller';
+import { RedisHealthIndicator } from './redis.health';
+
+@Module({
+  imports: [TerminusModule, PrismaModule, ConfigModule],
+  controllers: [HealthController],
+  providers: [PrismaHealthIndicator, RedisHealthIndicator],
+})
+export class HealthModule {}
---
+++ src/resources/healthcheck/redis.health.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import {
+  HealthIndicator,
+  HealthIndicatorResult,
+  HealthCheckError,
+} from '@nestjs/terminus';
+import { ConfigService } from '@nestjs/config';
+import Redis from 'ioredis';
+
+@Injectable()
+export class RedisHealthIndicator extends HealthIndicator {
+  private redisClient: Redis;
+
+  constructor(private configService: ConfigService) {
+    super();
+    this.redisClient = new Redis({
+      host: this.configService.get<string>('REDIS_HOST'),
+      port: this.configService.get<number>('REDIS_PORT'),
+    });
+  }
+
+  async isHealthy(key: string): Promise<HealthIndicatorResult> {
+    try {
+      const result = await this.redisClient.ping();
+      const isHealthy = result === 'PONG';
+      return this.getStatus(key, isHealthy);
+    } catch (error) {
+      throw new HealthCheckError(
+        'Redis check failed',
+        this.getStatus(key, false),
+      );
+    }
+  }
+}
