0a2f2bb09cc925ccc8834a3aaa483c1ad968ad11
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2024-10-01 23:56:46 -0300

    [Feature] Add DTOs for Label entity
    - Created `CreateLabelDto` for handling label creation with validation using `class-validator`.
    - Added `LabelDto` to structure label data responses, including transformations for optional fields.
    - Introduced `UpdateLabelDto` extending `CreateLabelDto` to support partial updates.

---
+++ src/resources/labels/dto/create-label.dto.ts
@@ -0,0 +1,37 @@
+import { LabelRegistrationStatus, LabelStatus } from '@prisma/client';
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class CreateLabelDto {
+  @IsNumber()
+  clientId: number;
+
+  @IsEnum(LabelStatus)
+  status: LabelStatus;
+
+  @IsString()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  website?: string;
+
+  @IsNumber()
+  @IsOptional()
+  countryId?: number;
+
+  @IsEnum(LabelRegistrationStatus)
+  @IsOptional()
+  beatportStatus?: LabelRegistrationStatus;
+
+  @IsEnum(LabelRegistrationStatus)
+  @IsOptional()
+  traxsourceStatus?: LabelRegistrationStatus;
+
+  @IsString()
+  @IsOptional()
+  beatportUrl?: string;
+
+  @IsString()
+  @IsOptional()
+  traxsourceUrl?: string;
+}
